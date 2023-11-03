bc2eafb499ecbc183c031c018461eb7552746454
ayu0616 <ayumuogawa0616@gmail.com>
2023-11-03 11:52:37 +0900

    コンフィグを修正した

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    webpackDevMiddleware: (config) => {
-        config.watchOptions = {
-            //小さな値にすると、ポーリングの頻度が上がるので、重くなるっぽい。
-            //poll: 800,
-            poll: 5000,
-            aggregateTimeout: 300,
-        };
-    },
 };
 
 module.exports = nextConfig;
