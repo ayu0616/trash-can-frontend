73ef4fc1acf03539170d2b84b69ddcd93a5c65a0
ayu0616 <ayumuogawa0616@gmail.com>
2023-11-26 14:47:59 +0900

    docker-composeようにURLを変更した

--- util/constVars.ts
+++ util/constVars.ts
@@ -1,6 +1,6 @@
 export const BACKEND_URL = (() => {
     if (process.env.NODE_ENV === "development") {
-        return "http://127.0.0.1:3001";
+        return "http://flask:3001";
     } else {
         return "https://trash-can-backend-zhqzb2nbkq-an.a.run.app/";
     }
