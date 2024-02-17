71c318b16f0ca51402a900c9f24c0a26d61280aa
ayu0616 <ayumuogawa0616@gmail.com>
2024-02-17 19:44:15 +0900

    デザインを少し修正した

--- components/layout/MainContent.tsx
+++ components/layout/MainContent.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const MainContent = ({ children }: { children: ReactNode }) => {
-    return <div className="flex flex-1 overflow-y-auto m-6 flex-col">{children}</div>;
+    return <div className="flex flex-1 overflow-y-auto mx-4 md:mx-6 flex-col">{children}</div>;
 };
 
 export default MainContent;
